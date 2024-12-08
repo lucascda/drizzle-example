b79c2edad1c2fedfafa5c73a0cc036c04c292496
Lucas César <43433070+lucascda@users.noreply.github.com>
2024-12-08 19:06:58 -0300

    feat: patch, post, delete

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,16 @@ import { createServer } from "http";
 import { db, todos } from "./db";
 import { errorHandler } from "./utils";
 import { eq } from "drizzle-orm";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
+app.use(compression());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
@@ -39,24 +47,64 @@ app.get(
         .select()
         .from(todos)
         .where(eq(todos.id, req.params.id));
-      res.json(result);
+      res.status(result.length === 1 ? 200 : 404).json(result);
     } catch (error) {
       next(error);
     }
   }
 );
 
-app.post("/api/v1/todos", (req: Request, res: Response) => {
-  res.send("POST CREATE TODO");
-});
+app.post(
+  "/api/v1/todos",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = await db
+        .insert(todos)
+        .values({
+          task: req.body.task,
+          description: req.body.description,
+          ...(req.body.dueDate && { dueDate: new Date(req.body.dueDate) }),
+        })
+        .returning();
+      res.status(201).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-app.patch("/api/v1/todos/:id", (req: Request, res: Response) => {
-  res.send("UPDATE TODO BY ID");
-});
+app.patch(
+  "/api/v1/todos/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const today = new Date();
+      const result = await db
+        .update(todos)
+        .set({
+          isDone: req.body.isDone,
+          doneAt: today,
+          updatedAt: today,
+        })
+        .where(eq(todos.id, req.params.id))
+        .returning();
+      res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-app.delete("/api/v1/todos/:id", (req: Request, res: Response) => {
-  res.send("DELETE TODO BY ID");
-});
+app.delete(
+  "/api/v1/todos/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = await db.delete(todos).where(eq(todos.id, req.params.id));
+      res.status(result.rowCount === 1 ? 200 : 404).json();
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   errorHandler.handle(err, res);
